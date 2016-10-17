dd6fa86c904e28d4261789bf86c5d538f83d1b28
np123 <patenikh2@gmail.com>
2016-10-17 04:13:19 -0400

    First pass at refactoring and redeveloping PySolFC

---
+++ src/PyCards/tests/testDriver.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    from pysollib import init
+    init.init()
---
+++ src/PyCards/tests/testResources.py
@@ -0,0 +1,22 @@
+import unittest
+
+class Test_resources(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_normalCase(self):
+        pass
+
+    def test_noCardset(self):
+        pass
+
+    def test_noImages(self):
+        pass
+
+    def test_noTiles(self):
+        pass
+
+    def tearDown(self):
+        pass
+
